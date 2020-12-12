fa0da7ddc183d6d01813d33d764cb28576710478
betaSolver05609 <someindrasingh795@gmail.com>
2020-12-12 17:20:15 +0530

    BFS

--- Graph Algorithms/BFS.py
+++ Graph Algorithms/BFS.py
@@ -13,36 +13,33 @@ class Graph:
     
     def AddEdgeToGraph(self, u, v):
         self.graph[u].append(v)
-        
-    def dfs(self, v, temp, visited):
-        visited.add(v)
-        temp.append(v)
-        for neighbour in self.graph[v]:
-            if neighbour not in visited:
-                temp=self.dfs(neighbour, temp, visited)
-        return temp
     
-    def connectedComponent(self):
-        cc=[];
+    def bfs(self, v):
         visited=set();
-        for i in range(len(self.graph)):
-            if i not in visited:
-                temp=[]
-                cc.append(self.dfs(i, temp, visited))
-        return cc;
-
+        queue=[];
+        queue.append(v);
+        visited.add(v);
+        while queue:
+            print(queue)
+            startNode=queue.pop(0)
+            print(startNode, end = " Visited \n")
+            for neighbour in self.graph[startNode]:
+                if neighbour not in visited:
+                    queue.append(neighbour)
+                    visited.add(neighbour)
+            
 g=Graph();
 
 g.AddEdgeToGraph(0,1)
 g.AddEdgeToGraph(1,2)
 g.AddEdgeToGraph(2,3)
-#g.AddEdgeToGraph(3,4)
+g.AddEdgeToGraph(3,4)
 g.AddEdgeToGraph(4,5)
 g.AddEdgeToGraph(5,6)
 g.AddEdgeToGraph(1,3)
-#g.AddEdgeToGraph(2,4)
+g.AddEdgeToGraph(2,4)
 
-print(g.connectedComponent())
+print(g.bfs(0))        
         
         
         
